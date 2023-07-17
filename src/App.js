fc5aa698bf9b235514518c84d93fc251105c2835
양민석 <kurooru98@gmail.com>
2023-07-17 21:13:04 +0900

    routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from "./pages/landing";
 import Evaluate from "./pages/evaluate";
+import MakeTeam from "./pages/makeTeam";
+import TeamInfo from "./pages/teamInfo";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
         <Routes>
           <Route path={`/`} element={<Landing />}></Route>
           <Route path={`/evaluate`} element={<Evaluate />}></Route>
+          <Route path={`/makeTeam`} element={<MakeTeam />}></Route>
+          <Route path={`/teamInfo`} element={<TeamInfo />}></Route>
         </Routes>
       </Router>
   );
