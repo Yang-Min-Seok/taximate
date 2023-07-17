e1b0149e9b3d90934777108e444409c9a52522da
양민석 <kurooru98@gmail.com>
2023-07-17 15:28:54 +0900

    popup test implement

--- src/pages/landing/body.jsx
+++ src/pages/landing/body.jsx
@@ -1,9 +1,34 @@
 import { BodyDiv } from "./style";
+import { useState } from "react";
 
 function Body() {
+
+    // 팝업 구현
+    const [popUp, setPopUp] = useState(true);
+
+    const handleOnClick = (e) => {
+        const order = e.target.id;
+
+        if (order==='popUpLayout' || order==='closeBtn'){
+            setPopUp(false);
+        }
+
+        else if(order === 'kakaoLogin'){
+            console.log(order);
+        }
+    }
     return (
         <BodyDiv>
-            <h1>Hello</h1>
+            {popUp && 
+            <div id="popUpLayout" onClick={handleOnClick}>
+                <div id="popUp">
+                    <p>간편하게 카카오 계정으로</p>
+                    <p>로그인 하세요!</p>
+                    <div id="kakaoLogin"></div>
+                    <p id="closeBtn">닫기</p>
+                </div>
+            </div>
+            }
         </BodyDiv>
     );
 }
--- src/pages/landing/style.jsx
+++ src/pages/landing/style.jsx
@@ -1,5 +1,72 @@
 import styled from "styled-components";
-
+import kakaoImg from "../../assets/images/kakaotalk.png";
 export const BodyDiv = styled.div`
     
+    // pop up
+    #popUpLayout{
+        position:fixed;
+        background-color: #eee;
+        left:0;
+        top: 0;
+        height: 100%;
+        width: 100%;
+        margin: 0 auto;
+        z-index: 1;
+    }
+    #popUp{
+        position: absolute;
+        z-index: 2;
+        width: 70%;
+        height: 70%;
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        top: 15%;
+        background-color: #fff;
+        border-radius: 5px;
+        color: #FFD732;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        p{  
+            font-size: 120%;
+            margin: 0%;
+            line-height: 2;
+        }
+        p:nth-child(1){
+
+        }
+        p:nth-child(2){
+            
+        }
+
+        // kakao login btn
+        div::before{
+            content: "";
+            display: block;
+            width: 50px;;
+            height: 50px;
+            background-image: url(${kakaoImg});
+            background-position: center;
+            background-size: contain;
+            background-repeat: no-repeat;
+        }
+        div{
+            width: 50px;
+            margin: 0 auto;
+            margin-top: 3%;
+        }
+
+        p:nth-child(4){
+            font-size: 60%;
+            display: block;
+            background-color: #FFD732;
+            width: 15%;
+            text-align: center;
+            color: #fff;
+            margin-top: 10%;
+        }
+    }
 `
