eaba838b68781ee517db18010580136f89961a65
양민석 <kurooru98@gmail.com>
2023-09-11 21:17:49 +0900

    landing body framing (no img yet)

--- src/pages/landing/body.jsx
+++ src/pages/landing/body.jsx
@@ -1,8 +1,19 @@
 import { BodyDiv } from "./style";
 function Body() {
+    
+    // 카카오 로그인 구현
+    const handleLogin = () => {
+        const Rest_api_key = 'd679f25e59dbc97619baf1256489b449';
+        const redirect_uri = `http://localhost:3000`;
+        const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
+        window.location.href = kakaoURL;
+    }
+
     return(
         <BodyDiv>
-
+            <h1>최근 1주일간 <span>100</span>번의 매칭</h1>
+            <div></div>
+            <div onClick={handleLogin}></div>
         </BodyDiv>
     )
 }
--- src/pages/landing/style.jsx
+++ src/pages/landing/style.jsx
@@ -1,5 +1,29 @@
 import styled from "styled-components";
-
+import KakaoLogin from "../../assets/images/kakaoLogin.png";
 export const BodyDiv = styled.div`
-    
+    h1{
+        text-align: center;
+        font-size: 170%;
+        font-weight: 500;
+        margin: 0;
+        line-height: 3;
+    }
+    h1 span{
+        color: red;
+        font-weight: 700;
+    }
+    div:nth-child(2){
+        width: 80%;
+        height: 200px;
+        margin: 0 auto;
+    }
+    div:nth-child(3){
+        width: 50%;
+        height: 80px;
+        margin: 0 auto;
+        background-image: url(${KakaoLogin});
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: contain;
+    }
 `
