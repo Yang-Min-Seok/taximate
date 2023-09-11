7a8e319e0070e3f55ef6a654fc5e5ed4deaf2aca
양민석 <kurooru98@gmail.com>
2023-09-11 21:01:17 +0900

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from "./pages/landing";
+import Option from "./pages/option";
+import Create from "./pages/create";
+import Map from "./pages/map";
+import Name from "./pages/name";
+import Mate from "./pages/mate";
+import Path from "./pages/path";
+import Rate from "./pages/rate";
 
 function App() {
   return (
       <Router>
         <Routes>
           <Route path={`/`} element={<Landing />}></Route>
+          <Route path={`/option`} element={<Option />}></Route>
+          <Route path={`/create`} element={<Create />}></Route>
+          <Route path={`/map`} element={<Map />}></Route>
+          <Route path={`/name`} element={<Name />}></Route>
+          <Route path={`/mate`} element={<Mate />}></Route>
+          <Route path={`/path`} element={<Path />}></Route>
+          <Route path={`/rate`} element={<Rate />}></Route>
         </Routes>
       </Router>
   );
