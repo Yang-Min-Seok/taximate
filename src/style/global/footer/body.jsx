ab305cf8ca70b6b0326dc258089ef929b031a2a2
양민석 <kurooru98@gmail.com>
2023-09-11 21:01:30 +0900

    header framing

--- src/style/global/footer/body.jsx
+++ src/style/global/footer/body.jsx
@@ -3,7 +3,9 @@ import { BodyDiv } from "./style";
 function Body() {
     return (
         <BodyDiv>
-            
+            <div id="back"></div>
+            <div id="logo"></div>
+            <div id="profileImg"></div>
         </BodyDiv>
     );
 }
