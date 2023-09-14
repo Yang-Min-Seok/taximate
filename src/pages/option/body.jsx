d9c963383f7b46638490443f65589ba7eea21cae
양민석 <kurooru98@gmail.com>
2023-09-14 15:20:29 +0900

    option framing

--- src/pages/option/body.jsx
+++ src/pages/option/body.jsx
@@ -1,8 +1,30 @@
 import { BodyDiv } from "./style";
+import { useNavigate } from "react-router-dom"; 
 function Body() {
-    return(
-        <BodyDiv>
+    
+    // option 클릭 시
+    const navigate = useNavigate();
+    const handleOnClick = (e) => {
+        const order = e.target.id;
+        if (order) {
+            navigate(`/${order}`);
+        }
+    }
 
+    return(
+        <BodyDiv onClick={handleOnClick}>
+            <div id="create">
+                <p>내가 만들기</p>
+                <p>함께 탈 택시를 내가 만들어요!</p>
+            </div>
+            <div id="map">
+                <p>지도에서 찾기</p>
+                <p>가까운 출발지를 지도에서 확인해요!</p>
+            </div>
+            <div id="name">
+                <p>역 이름으로 찾기</p>
+                <p>역이름을 기준으로 검색해요!</p>
+            </div>
         </BodyDiv>
     )
 }
--- src/pages/option/style.jsx
+++ src/pages/option/style.jsx
@@ -1,5 +1,110 @@
 import styled from "styled-components";
+import create from "../../assets/images/create.jpg";
+import map from "../../assets/images/map.jpg";
+import name from "../../assets/images/name.jpg";
 
 export const BodyDiv = styled.div`
-    
+    margin: 30px 0px;
+    /* common */
+    div{
+        width: 70%;
+        height: 100px;
+        margin: 0 auto;
+        padding: 20px;
+        box-sizing: border-box;
+        border: 2px solid #03588C;
+        border-radius: 10px;
+    }
+    div p{
+        margin: 0;
+    }
+
+    div:nth-child(1){
+        margin-bottom: 20px;
+        position: relative;
+    }
+    div:nth-child(1)::before{
+        content: "";
+        background-image: url(${create});
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        position: absolute;
+        opacity: 0.3;
+        top: 0px;
+        left: 0px;
+        right: 0px;
+        bottom: 0px;
+    }
+    div:nth-child(1) p{
+
+    }
+    div:nth-child(1) p:nth-child(1){
+        font-size: 120%;
+        font-weight: 700;
+    }
+    div:nth-child(1) p:nth-child(2){
+        text-align: right;
+        font-size: 90%;
+        line-height: 3;
+    }
+
+    div:nth-child(2){
+        margin-bottom: 20px;
+        position: relative;
+    }
+    div:nth-child(2)::before{
+        content: "";
+        background-image: url(${map});
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        position: absolute;
+        opacity: 0.3;
+        top: 0px;
+        left: 0px;
+        right: 0px;
+        bottom: 0px;
+    }
+    div:nth-child(2) p{
+        
+    }
+    div:nth-child(2) p:nth-child(1){
+        font-size: 120%;
+        font-weight: 700;
+    }
+    div:nth-child(2) p:nth-child(2){
+        text-align: right;
+        font-size: 90%;
+        line-height: 3;
+    }
+
+    div:nth-child(3){
+        position: relative;
+    }
+    div:nth-child(3)::before{
+        content: "";
+        background-image: url(${name});
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        position: absolute;
+        opacity: 0.3;
+        top: 0px;
+        left: 0px;
+        right: 0px;
+        bottom: 0px;
+    }
+    div:nth-child(3) p{
+        
+    }
+    div:nth-child(3) p:nth-child(1){
+        font-size: 120%;
+        font-weight: 700;
+    }
+    div:nth-child(3) p:nth-child(2){
+        text-align: right;
+        font-size: 90%;
+        line-height: 3;
+    }
 `
