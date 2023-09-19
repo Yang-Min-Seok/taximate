bf91d20f66b978afb9fbe6f6554e0db37728ef4f
양민석 <kurooru98@gmail.com>
2023-09-19 19:34:49 +0900

    rate framing

--- src/pages/rate/body.jsx
+++ src/pages/rate/body.jsx
@@ -1,8 +1,65 @@
 import { BodyDiv } from "./style";
 function Body() {
+    const handleOnChangeStar = (e) => {
+        const selectedStarNum = e.target.id.split('star')[0];
+        const selectedStarMemberId = e.target.id.split('star')[1];
+
+        for(let i=1; i<=5; i++){
+            const target = document.getElementById(`${i}star${selectedStarMemberId}Label`);
+            target.style.color = '#000';
+        }
+        for(let i=1; i<=selectedStarNum; i++){
+            const target = document.getElementById(`${i}star${selectedStarMemberId}Label`);
+            target.style.color = 'gold';
+        }
+
+    }
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        console.log(e);
+    }
     return(
         <BodyDiv>
-
+            <h3>함께한 메이트를 평가해주세요!</h3>
+            <form onSubmit={handleOnSubmit}>
+                <div>
+                    <div>
+                        <div></div>
+                        <p>양민석</p>
+                    </div>
+                    <div>
+                        <input type="radio" name="1" id="1star1" onChange={handleOnChangeStar}/>
+                        <input type="radio" name="1" id="2star1" onChange={handleOnChangeStar}/>
+                        <input type="radio" name="1" id="3star1" onChange={handleOnChangeStar}/>
+                        <input type="radio" name="1" id="4star1" onChange={handleOnChangeStar}/>
+                        <input type="radio" name="1" id="5star1" onChange={handleOnChangeStar}/>
+                        <label htmlFor="1star1" id="1star1Label">☆</label>
+                        <label htmlFor="2star1" id="2star1Label">☆</label>
+                        <label htmlFor="3star1" id="3star1Label">☆</label>
+                        <label htmlFor="4star1" id="4star1Label">☆</label>
+                        <label htmlFor="5star1" id="5star1Label">☆</label>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div></div>
+                        <p>정원준</p>
+                    </div>
+                    <div>
+                        <input type="radio" name="2" id="1star2" onChange={handleOnChangeStar}/>
+                        <input type="radio" name="2" id="2star2" onChange={handleOnChangeStar}/>
+                        <input type="radio" name="2" id="3star2" onChange={handleOnChangeStar}/>
+                        <input type="radio" name="2" id="4star2" onChange={handleOnChangeStar}/>
+                        <input type="radio" name="2" id="5star2" onChange={handleOnChangeStar}/>
+                        <label htmlFor="1star2" id="1star2Label">☆</label>
+                        <label htmlFor="2star2" id="2star2Label">☆</label>
+                        <label htmlFor="3star2" id="3star2Label">☆</label>
+                        <label htmlFor="4star2" id="4star2Label">☆</label>
+                        <label htmlFor="5star2" id="5star2Label">☆</label>
+                    </div>
+                </div>
+                <button type="submit">평가완료</button>
+            </form>
         </BodyDiv>
     )
 }
--- src/pages/rate/style.jsx
+++ src/pages/rate/style.jsx
@@ -1,5 +1,60 @@
 import styled from "styled-components";
-
 export const BodyDiv = styled.div`
-    
+    h3{
+        text-align: center;
+        margin: 0;
+        line-height: 3;
+    }
+
+    form{
+        width: 80%;
+        margin: 0 auto;
+    }
+    form div{
+        display: flex;
+        height: 74px;
+    }
+    form div div:nth-child(1){
+        display: block;
+        height: 100%;
+    }
+    form div div:nth-child(1) div:nth-child(1){
+        height: 44px;
+        width: 50px;
+        height: 50px;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: contain;
+        border-radius: 50%;
+    }
+    form div div:nth-child(1) p:nth-child(2){
+        margin: 0;
+        text-align: center;
+        line-height: 1.5;
+    }
+    form div div:nth-child(2){
+        width: calc(100% - 50px);
+        justify-content: space-around;
+    }
+    form div div:nth-child(2) input{
+        display: none;
+    }
+    form div div:nth-child(2) label{
+        line-height: 74px;
+        font-size: 200%;
+    }
+
+    form button{
+        display: block;
+        width: 40%;
+        line-height: 2;
+        border: 2px solid #03588C;
+        border-radius: 10px;
+        font-size: 130%;
+        font-weight: 600;
+        background-color: #fff;
+        margin: 30px auto;
+        transition: all 0.5s;
+        color: #6DD93B;
+    }
 `
