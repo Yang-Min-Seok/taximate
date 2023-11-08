4b6f94ce55b3a3f1504258f396054596045e2f2b
양민석 <kurooru98@gmail.com>
2023-11-08 15:38:37 +0900

    name framing

--- src/pages/name/body.jsx
+++ src/pages/name/body.jsx
@@ -1,8 +1,22 @@
 import { BodyDiv } from "./style";
 function Body() {
+    const handleOnClick = (e) => {
+        e.preventDefault();
+        console.log('search');
+    }
     return(
         <BodyDiv>
-            
+            <h3>키워드 검색 <span>(다중 선택 가능)</span></h3>
+            <div>
+                <p>출발지</p>
+                <input type="text" placeholder="ex) 서울 용산 시청"/>
+            </div>
+            <div>
+                <p>도착지</p>
+                <input type="text" placeholder="ex) 강남 신논현 교대"/>
+            </div>
+            <p>* 여러 키워드로 검색 시 <span>공백(' ')</span> 으로 구분</p>
+            <button onClick={handleOnClick}>검색하기</button>
         </BodyDiv>
     )
 }
--- src/pages/name/style.jsx
+++ src/pages/name/style.jsx
@@ -2,4 +2,83 @@ import styled from "styled-components";
 
 export const BodyDiv = styled.div`
     
+    h3:nth-child(1){
+        margin: 0;
+        text-align: center;
+        line-height: 3;
+        font-size: 150%;
+    }
+    h3:nth-child(1) span{
+        color: #03588C;
+        text-decoration: underline;
+    }
+
+    div:nth-child(2){
+        width: 80%;
+        margin: 0 auto;
+    }
+    div:nth-child(2) p{
+        margin: 0;
+        font-size: 120%;
+        line-height: 2;
+    }
+    div:nth-child(2) input{
+        padding: 0;
+        padding-left: 3%;
+        box-sizing: border-box;
+        line-height: 2;
+        font-size: 110%;
+        width: 100%;
+        border: 2px solid #6DD93B;
+        border-radius: 10px;
+    }
+
+    div:nth-child(3){
+        width: 80%;
+        margin: 0 auto;
+        margin-top: 20px;
+    }
+    div:nth-child(3) p{
+        margin: 0;
+        font-size: 120%;
+        line-height:2;
+    }
+    div:nth-child(3) input{
+        padding: 0;
+        padding-left: 3%;
+        box-sizing: border-box;
+        line-height: 2;
+        font-size: 110%;
+        width: 100%;
+        border: 2px solid #6DD93B;
+        border-radius: 10px;
+    }
+
+
+    p:nth-child(4){
+        margin: 0;
+        color: #03588C;
+        text-align: center;
+        line-height: 3;
+        font-size: 110%;
+    }
+    p:nth-child(4) span{
+        text-decoration: underline;
+        color: red;
+    }
+
+    button:nth-child(5){
+        display: block;
+        width: 40%;
+        line-height: 2;
+        border: 2px solid #03588C;
+        border-radius: 10px;
+        font-size: 130%;
+        font-weight: 600;
+        background-color: #fff;
+        margin: 0 auto;
+        transition: all 0.5s;
+        color: #6DD93B;
+        padding: 0;
+    }
 `
